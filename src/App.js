a6c6e7eab339d3f0c3d625194374efeb86877936
MathieuGAUBAL <b_jayson@hotmail.com>
2022-03-21 13:54:41 +0100

    challenge ok

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import './scss/styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-main">
+      <div className='container-card'>
+        <div className='card'>
+          <div className='card-img'>
+
+            <div className='container-img'>
+              <img className='img-qrcode' src='/image-qr-code.png' alt="qrcode" />
+            </div>
+
+            <div className='container-text'>
+              <h1>Improve your front-end skills by building projects</h1>
+              <p>
+                Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+              </p>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="attribution">
+        Challenge by <a rel="noreferrer" href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+        Coded by <a rel="noreferrer" href="https://github.com/matcoco/qr-code-component">Github Matcoco</a>.
+      </div>
     </div>
   );
 }
